b4af2e90270624c5dca5bb8055bd03296fa738b5
Haroun B <haroun@harryb.dev>
2022-05-20 16:06:26 +0200

    save for later

--- js/main.js
+++ js/main.js
@@ -7,6 +7,23 @@ class GameBoard {
   }
   _createCells() {
     // iteration 1
+    this.element = document.querySelector(`.grid`);
+
+    const cells = [];
+
+    for (let i = 0; i < this.width * this.height; i++) {
+      const cell = document.createElement(`div`);
+
+      cell.classList.add(`cell`);
+      cell.dataset.index = i
+
+      cells.push(cell);
+
+      cell.textContent = i; // ! to remove later
+      this.element.appendChild(cell);
+    }
+
+    return cells;
   }
 }
 
@@ -32,9 +49,32 @@ const inventory = {
   element: null,
   add() {
     // iteration 3
+    if (!this.element) {
+      this.element = document.querySelector(`.inventory`);
+    }
+
+    let collected = 0;
+    this.clear();
+    collectibles.forEach(collectible => {
+      if (collectible.isCollected) {
+        collected++;
+
+        const collectibleDiv = document.createElement(`div`);
+        collectibleDiv.classList.add(collectible.className);
+        
+        this.element.appendChild(collectibleDiv);
+        console.log(this.element, collectibleDiv);
+      }
+    });
+
+    if (collected === collectibles.length) {
+      console.log(`won`);
+      game.win();
+    }
   },
   clear() {
     // iteration 3 (reset behaviour)
+    this.element.innerHTML = ``;
   },
 }
 
@@ -46,12 +86,18 @@ class Collectible {
   }
   hide() {
     // reset behaviour
+    this.cell.classList.remove(this.className);
   }
   collect() {
     // iteration 4
+    this.isCollected = true;
+
+    this.hide();
+    inventory.add();
   }
   display() {
     // iteration 2
+    this.cell.classList.add(this.className);
   }
 }
 
@@ -66,10 +112,21 @@ const collectibles = [
 
 function distributeCollectibles() {
   // iteration 2
+  const randomlySelectedCells = getRandomSelection(collectibles.length, board.cells);
+
+  collectibles.forEach((collectible, index) => {
+    const cell = randomlySelectedCells[index];
+    
+    collectible.cell = cell;
+    collectible.display();
+  });
 }
 
 function getRandomUnoccupiedCell() {
   // iteration 3
+  const unoccupiedCells = board.cells.filter(cell => cell.className === `cell`);
+
+  return getRandomSelection(1, unoccupiedCells)[0];
 }
 
 const player = {
@@ -77,20 +134,64 @@ const player = {
   cell: getRandomUnoccupiedCell(),
   show() {
     // iteration 3
+    this.cell.classList.add(this.className);
   },
   hide() {
     // iteration 3
+    this.cell.classList.remove(this.className);
   },
   move(direction) {
     // iteration 3
+    if (!this.canMove(direction)) {
+      return;
+    }
+
+    this.hide();
+    const cellIndex = parseInt(this.cell.dataset.index);
+    switch (direction) {
+      case `up`:
+        this.cell = board.cells[cellIndex - board.width];
+        break;
+      case `down`:
+        this.cell = board.cells[cellIndex + board.width];
+        break;
+      case `left`:
+        this.cell = board.cells[cellIndex - 1];
+        break;
+      case `right`:
+        this.cell = board.cells[cellIndex + 1];
+        break;
+    }
+
+    this.show();
+    this._detectCollisions();
   },
   canMove(direction) {
     // hint for iteration 3: make move behaviour conditional
+    const cellIndex = parseInt(this.cell.dataset.index);
+    switch (direction) {
+      case `up`:
+        return cellIndex - board.width >= 0;
+      case `down`:
+        return cellIndex + board.width < board.width * board.height;
+      case `left`:
+        return cellIndex % board.width !== 0;
+      case `right`:
+        return (cellIndex + 1) % board.width !== 0;
+    }
   },
   _detectCollisions() {
     // iteration 4
     // how do we detect collisions with items
     // when do we call this?
+    if (this.cell.className !== `cell player`) {
+      for (let i = 0; i < collectibles.length; i++) {
+        if (collectibles[i].cell.dataset.index === this.cell.dataset.index) {
+          collectibles[i].collect();
+          return;
+        }
+      }
+    }
   },
 }
 
@@ -102,16 +203,23 @@ const game = {
   winAudio: null,
   win() {
     // iteration 4
+    document.getElementById(`anthem`).play();
   },
   start() {
     // iteration 2
     // distribute the items
+    distributeCollectibles();
     // iteration 3
     // show the player
+    player.show();
     // iteration 4
     // reset the inventory
     // iteration 5
     // reset the music
+
+
+    this.isStarted = true;
+
   },
 }
 
@@ -119,6 +227,7 @@ const startButton = document.querySelector('button#start')
 startButton.addEventListener('click', () => {
   // iteration 2
   // start the game
+  game.start();
 })
 
 document.addEventListener('keydown', (event) => {
